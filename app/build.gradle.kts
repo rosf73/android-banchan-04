2a8805a2fcba2844404bce3951e9b13aa9fada26
bn-tw2020 <skaxodn97@gmail.com>
2022-08-09 10:11:41 +0900

    chore: 버전 관리 설정
    - Groovy to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        applicationId = Config.applicationId
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Lib.Androidx.core)
+    implementation(Lib.Androidx.appcompat)
+    implementation(Lib.Androidx.googleMaterial)
+    implementation(Lib.Androidx.constraintLayout)
+    testImplementation(Lib.Test.junit4)
+    androidTestImplementation(Lib.Test.androidxJunit)
+    androidTestImplementation(Lib.Test.espressoCore)
+}
--- app/src/main/java/com/woowa/banchan/MainActivity.kt
+++ app/src/main/java/com/woowa/banchan/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.woowa.banchan
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
