ac79cf0d9a62e2337da8446a0f21fa3e34553231
bn-tw2020 <skaxodn97@gmail.com>
2022-08-12 00:19:42 +0900

    feat: 데이터와 UI 연결
    - 이미지 로딩 전 보여주기 위한 커스텀 백그라운드 설정
    - 장바구니 버튼 클릭 시, Product 객체 넘겨주기
    - 넘어온 데이터 데이터바인딩

--- app/src/main/java/com/woowa/banchan/ui/common/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/woowa/banchan/ui/common/ImageViewBindingAdapter.kt
@@ -3,8 +3,12 @@ package com.woowa.banchan.ui.common
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.woowa.banchan.R
 
 @BindingAdapter("src")
 fun ImageView.setSource(url: String) {
-    Glide.with(context).load(url).into(this)
+    Glide.with(context)
+        .load(url)
+        .placeholder(R.drawable.iv_placeholder)
+        .into(this)
 }
