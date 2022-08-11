38e33ad55b1c67acf6db329524a6b8edfe290fcf
rosf73 <alswlzhsclq@naver.com>
2022-08-11 11:22:35 +0900

    feature: 앱 바 디자인
    - 커스텀 앱 바 제작하여 ViewBinding으로 연결

---
+++ app/src/main/java/com/woowa/banchan/ui/common/BanchanAppbar.kt
@@ -0,0 +1,54 @@
+package com.woowa.banchan.ui.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import com.woowa.banchan.R
+import com.woowa.banchan.databinding.ViewAppBarBinding
+
+class BanchanAppbar(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private val binding: ViewAppBarBinding =
+        DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.view_app_bar, this, true)
+
+    private val navigationIcon = binding.ivNavigationIcon
+    private val title = binding.tvTitle
+    private val actionFirst = binding.ivActionFirst
+    private val actionSecond = binding.ivActionSecond
+
+    init {
+        initAttrs(attrs)
+    }
+
+    private fun initAttrs(attrs: AttributeSet) {
+        context.theme.obtainStyledAttributes(attrs, R.styleable.BanchanAppBar, 0, 0).apply {
+            setTitle(getString(R.styleable.BanchanAppBar_title))
+            setNavigationIcon(getResourceId(R.styleable.BanchanAppBar_navigation_icon, 0))
+            setActionFirst(getResourceId(R.styleable.BanchanAppBar_action_first, 0))
+            setActionSecond(getResourceId(R.styleable.BanchanAppBar_action_second, 0))
+        }
+    }
+
+    private fun setActionSecond(resourceId: Int) {
+        if (resourceId != 0) this.actionSecond.setImageResource(resourceId)
+        else actionSecond.isVisible = isGone
+    }
+
+    private fun setActionFirst(resourceId: Int) {
+        if (resourceId != 0) this.actionFirst.setImageResource(resourceId)
+        else actionFirst.isVisible = isGone
+    }
+
+    private fun setNavigationIcon(resourceId: Int) {
+        if (resourceId != 0) this.navigationIcon.setImageResource(resourceId)
+    }
+
+    private fun setTitle(title: String?) {
+        if (title != null) this.title.text = title
+    }
+
+}
