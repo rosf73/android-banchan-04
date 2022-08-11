7ba38330b63f548f1fc9af2e1f76f76dadbb257c
rosf73 <alswlzhsclq@naver.com>
2022-08-11 21:52:28 +0900

    feat: repository 테스트 코드 추가
    - 메인요리 테스트 코드 작성
      - FakeDataSource를 만듦
      - Fake Test Double을 이용한 getProducts() 테스트 코드 작성

--- app/src/test/java/com/woowa/banchan/data/remote/repository/BanchanRepositoryImplTest.kt
+++ app/src/test/java/com/woowa/banchan/data/remote/repository/BanchanRepositoryImplTest.kt
@@ -1,16 +1,13 @@
 package com.woowa.banchan.data.remote.repository
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.woowa.banchan.data.remote.datasource.BanchanDataSource
-import com.woowa.banchan.data.remote.dto.CategoryDto
-import com.woowa.banchan.data.remote.dto.PlanResponse
-import com.woowa.banchan.data.remote.dto.ProductDto
-import com.woowa.banchan.data.remote.dto.toCategory
+import com.woowa.banchan.data.remote.dto.*
 import com.woowa.banchan.data.remote.network.BanchanService
-import com.woowa.banchan.domain.entity.Category
 import com.woowa.banchan.domain.repository.BanchanRepository
 import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -21,11 +18,47 @@ class BanchanRepositoryImplTest {
     private lateinit var banchanDataSource: BanchanDataSource
     private lateinit var banchanRepository: BanchanRepository
 
+    private lateinit var fakeProductResponse: ProductsResponse
+    private lateinit var fakeDataSource: FakeDataSource
+    private lateinit var banchanRepositoryForFakeTest: BanchanRepository
+
     @Before
     fun setUp() {
+        // mock Test Double
         banchanService = mockk(relaxed = true)
         banchanDataSource = mockk(relaxed = true)
         banchanRepository = BanchanRepositoryImpl(banchanDataSource)
+
+        // fake Test Double
+        fakeProductResponse = ProductsResponse(
+            statusCode = 200,
+            products = listOf(
+                ProductDto(
+                    detailHash = "HBDEF",
+                    image = "http://public.codesquad.kr/jk/storeapp/data/main/1155_ZIP_P_0081_T.jpg",
+                    alt = "오리 주물럭_반조리",
+                    deliveryType = listOf("새벽배송", "전국택배"),
+                    title = "오리 주물럭_반조리",
+                    description = "감칠맛 나는 매콤한 양념",
+                    nPrice = "15,800원",
+                    sPrice = "12,640원",
+                    badge = listOf("런칭특가")
+                ),
+                ProductDto(
+                    detailHash = "HDF73",
+                    image = "http://public.codesquad.kr/jk/storeapp/data/main/310_ZIP_P_0012_T.jpg",
+                    alt = "잡채",
+                    deliveryType = listOf("새벽배송", "전국택배"),
+                    title = "잡채",
+                    description = "탱글한 면발과 맛깔진 고명이 가득",
+                    nPrice = "12,900원",
+                    sPrice = "11,610원",
+                    badge = listOf("이벤트특가")
+                )
+            )
+        )
+        fakeDataSource = FakeDataSource(products = fakeProductResponse)
+        banchanRepositoryForFakeTest = BanchanRepositoryImpl(fakeDataSource)
     }
 
     @Test
@@ -56,20 +89,21 @@ class BanchanRepositoryImplTest {
         val banchanCategory = Result.success(expectedResponse)
         coEvery { banchanDataSource.getPlan() } returns banchanCategory
 
-        // when, then
-        var actualCategory: List<Category>?
-        banchanRepository.getPlan().collect {
-            actualCategory = it.getOrNull()
-            Truth.assertThat(actualCategory)
-                .isEqualTo(expectedResponse.categories.map { it.toCategory() })
-        }
+        // when
+        val actualCategory = banchanRepository.getPlan().first().getOrNull()
+
+        // then
+        assertThat(actualCategory)
+            .isEqualTo(expectedResponse.categories.map { it.toCategory() })
     }
 
     @Test
-    fun getProducts() {
-    }
+    fun `메인요리의 상품 메뉴를 가져온다`() = runBlocking {
+        // when
+        val products = banchanRepositoryForFakeTest.getProducts("main").first().getOrNull()
 
-    @Test
-    fun getDetailProduct() {
+        // then
+        assertThat(products)
+            .isEqualTo(fakeProductResponse.products.map { it.toProduct() })
     }
 }
---
+++ app/src/test/java/com/woowa/banchan/data/remote/repository/FakeDataSource.kt
@@ -0,0 +1,35 @@
+package com.woowa.banchan.data.remote.repository
+
+import com.woowa.banchan.data.remote.datasource.BanchanDataSource
+import com.woowa.banchan.data.remote.dto.DetailResponse
+import com.woowa.banchan.data.remote.dto.PlanResponse
+import com.woowa.banchan.data.remote.dto.ProductsResponse
+import com.woowa.banchan.domain.exception.NotFoundProductsException
+
+class FakeDataSource(
+    private val plan: PlanResponse? = null,
+    private val products: ProductsResponse? = null,
+    private val details: DetailResponse? = null
+): BanchanDataSource {
+
+    override suspend fun getPlan(): Result<PlanResponse> {
+        if (plan != null) {
+            return Result.success(plan)
+        }
+        return Result.failure(NotFoundProductsException())
+    }
+
+    override suspend fun getProducts(type: String): Result<ProductsResponse> {
+        if (products != null) {
+            return Result.success(products)
+        }
+        return Result.failure(NotFoundProductsException())
+    }
+
+    override suspend fun getDetailProduct(hash: String): Result<DetailResponse> {
+        if (details != null) {
+            return Result.success(details)
+        }
+        return Result.failure(NotFoundProductsException())
+    }
+}
