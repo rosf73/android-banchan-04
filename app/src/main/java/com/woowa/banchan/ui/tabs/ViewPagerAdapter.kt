abf022ef9db9ca92ae381f1bef854c34a7de7219
rosf73 <alswlzhsclq@naver.com>
2022-08-13 20:08:42 +0900

    fix: 화면 전환 시 프래그먼트 전환 객체를 보존하지 않는 문제 해결
    - 단순히 MainFragment에 public 메소드로 선언하여 parentFragment의 멤버 함수를 호출

--- app/src/main/java/com/woowa/banchan/ui/tabs/ViewPagerAdapter.kt
+++ app/src/main/java/com/woowa/banchan/ui/tabs/ViewPagerAdapter.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.woowa.banchan.ui.main.Tab
-import com.woowa.banchan.ui.tabs.common.OnClickMenu
 import com.woowa.banchan.ui.tabs.home.HomeFragment
 import com.woowa.banchan.ui.tabs.maindish.MainDishFragment
 import com.woowa.banchan.ui.tabs.side.SideFragment
@@ -13,27 +12,18 @@ import com.woowa.banchan.ui.tabs.soup.SoupFragment
 
 class ViewPagerAdapter(
     fm: FragmentManager,
-    lifecycle: Lifecycle,
-    private val onClickMenu: OnClickMenu
+    lifecycle: Lifecycle
 ) : FragmentStateAdapter(fm, lifecycle) {
 
     override fun getItemCount(): Int = SIZE
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            Tab.HOME.ordinal -> {
-                val homeFragment = HomeFragment()
-                homeFragment.setOnClickMenu(onClickMenu)
-                homeFragment
-            }
+            Tab.HOME.ordinal -> HomeFragment()
             Tab.MAIN_DISH.ordinal -> MainDishFragment()
             Tab.SOUP.ordinal -> SoupFragment()
             Tab.SIDE.ordinal -> SideFragment()
-            else -> {
-                val homeFragment = HomeFragment()
-                homeFragment.setOnClickMenu(onClickMenu)
-                homeFragment
-            }
+            else -> HomeFragment()
         }
     }
 
