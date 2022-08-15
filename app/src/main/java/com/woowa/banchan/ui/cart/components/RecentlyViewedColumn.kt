6b316e254d46110266e19b283f30ceb8ab53048b
rosf73 <alswlzhsclq@naver.com>
2022-08-16 00:24:08 +0900

    ui: 최근 본 상품 미리보기 ui 구성 완료
    - 7글자를 넘어가면 글자를 자름

--- app/src/main/java/com/woowa/banchan/ui/cart/components/RecentlyViewedColumn.kt
+++ app/src/main/java/com/woowa/banchan/ui/cart/components/RecentlyViewedColumn.kt
@@ -7,22 +7,31 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomStart
+import androidx.compose.ui.Alignment.Companion.CenterEnd
+import androidx.compose.ui.Alignment.Companion.CenterStart
 import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.TopStart
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.woowa.banchan.R
 import com.woowa.banchan.ui.cart.TestRecently
 import com.woowa.banchan.ui.cart.testRecentlyList
+import com.woowa.banchan.utils.substringShort
 
 @Composable
 fun RecentlyViewedColumn(
     modifier: Modifier = Modifier,
     recentlyList: List<TestRecently>
 ) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier.padding(16.dp, 32.dp)) {
         RecentlyViewedHeader(onViewTotalClick = {})
 
         RecentlyViewedRow(recentlyList)
@@ -33,7 +42,7 @@ fun RecentlyViewedColumn(
 private fun RecentlyViewedHeader(
     onViewTotalClick: () -> Unit
 ) {
-    Row(modifier = Modifier.padding(20.dp, 8.dp)) {
+    Row(modifier = Modifier.padding(0.dp, 8.dp)) {
         Text(
             modifier = Modifier
                 .weight(1f)
@@ -55,7 +64,7 @@ private fun RecentlyViewedHeader(
 private fun RecentlyViewedRow(
     recentlyList: List<TestRecently>
 ) {
-    LazyRow(modifier = Modifier.padding(8.dp)) {
+    LazyRow(modifier = Modifier) {
         items(recentlyList) { item ->
             RecentlyViewedItem(
                 modifier = Modifier
@@ -76,10 +85,15 @@ private fun RecentlyViewedItem(
             modifier = Modifier.size(120.dp),
             url = item.thumb)
 
-        Text(text = item.name)
-        Text(text = item.sPrice)
-        Text(text = item.nPrice)
-        Text(text = item.viewedAt)
+        Text(text = item.name.substringShort(), fontWeight = FontWeight(500), color = colorResource(R.color.black), maxLines = 1)
+        Row {
+            Text(text = item.sPrice, fontWeight = FontWeight(500), color = colorResource(R.color.black))
+            Text(text = item.nPrice,
+                fontWeight = FontWeight(400), color = colorResource(R.color.gray_default), fontSize = 12.sp,
+                style = TextStyle(textDecoration = TextDecoration.LineThrough))
+        }
+        Text(text = item.viewedAt,
+            fontWeight = FontWeight(400), color = colorResource(R.color.gray_default), fontSize = 12.sp)
     }
 }
 
