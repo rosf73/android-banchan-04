8b1d52f1785538fdac15a261a2f91b928c0471dd
bn-tw2020 <skaxodn97@gmail.com>
2022-08-17 20:50:52 +0900

    feat: 데이터베이스 접근 Dao 생성

---
+++ app/src/main/java/com/woowa/banchan/data/local/dao/OrderDao.kt
@@ -0,0 +1,10 @@
+package com.woowa.banchan.data.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface OrderDao {
+    /*
+        findAll(): Flow<List<*>>
+     */
+}
---
+++ app/src/main/java/com/woowa/banchan/data/local/dao/RecentlyViewedDao.kt
@@ -0,0 +1,13 @@
+package com.woowa.banchan.data.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface RecentlyViewedDao {
+    /*
+          findAll(): Flow<List<*>>
+          findTop7ByViewedAtDesc(): Flow<List<*>>
+          updateById()
+          insert()
+     */
+}
