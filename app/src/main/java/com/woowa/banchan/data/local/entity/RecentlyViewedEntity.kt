1e4b1fe754e52f97053cc59686c7b210e237072b
bn-tw2020 <skaxodn97@gmail.com>
2022-08-17 20:50:24 +0900

    feat: 데이터베이스 엔티티 추가

---
+++ app/src/main/java/com/woowa/banchan/data/local/entity/RecentlyViewedEntity.kt
@@ -0,0 +1,16 @@
+package com.woowa.banchan.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recently_viewed")
+data class RecentlyViewedEntity(
+    @PrimaryKey @ColumnInfo(name = "id") val id: Long,
+    @ColumnInfo(name = "hash") val hash: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @ColumnInfo(name = "n_price") val nPrice: Int,
+    @ColumnInfo(name = "s_price") val sPrice: Int,
+    @ColumnInfo(name = "viewed_at") val viewedAt: Long
+)
