a463ac9fce0a6c0d675b12066d57c7fdd75cdd00
bn-tw2020 <skaxodn97@gmail.com>
2022-08-18 01:25:32 +0900

    test: 장바구니 여러개 삭제, 삽입 테스트 코드

--- app/src/androidTest/java/com/woowa/banchan/data/local/CartDaoTest.kt
+++ app/src/androidTest/java/com/woowa/banchan/data/local/CartDaoTest.kt
@@ -1,7 +1,5 @@
 package com.woowa.banchan.data.local
 
-import android.util.Log
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,7 +12,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -22,8 +19,6 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CartDaoTest {
 
-    @get:Rule
-    var instantTaskExecutorRule = InstantTaskExecutorRule()
     private lateinit var database: AppDatabase
     private lateinit var dao: CartDao
     private val cart = CartEntity(
@@ -54,11 +49,48 @@ class CartDaoTest {
     }
 
     @Test
+    fun `장바구니에_상품을_여러개_넣을_수_있다`() = runTest {
+        var insertList = arrayOf(
+            cart, CartEntity(
+                2L,
+                "무진장 매운 오이무침",
+                "http://public.codesquad.kr/jk/storeapp/data/side/48_ZIP_P_5008_T.jpg",
+                2,
+                "6,000원",
+                false
+            )
+        )
+        dao.insertCart(*insertList)
+        val cartList = dao.findAll().first()
+
+        assertThat(cartList).hasSize(2)
+    }
+
+    @Test
     fun `장바구니에서_데이터를_삭제할_수_있다`() = runTest {
         dao.insertCart(cart)
         dao.deleteCart(cart)
 
         val cartList = dao.findAll().first()
+        assertThat(cartList).isEmpty()
+    }
+
+    @Test
+    fun `장바구니에서_데이터를_여러개_삭제할_수_있다`() = runTest {
+        var insertList = arrayOf(
+            cart, CartEntity(
+                2L,
+                "무진장 매운 오이무침",
+                "http://public.codesquad.kr/jk/storeapp/data/side/48_ZIP_P_5008_T.jpg",
+                2,
+                "6,000원",
+                false
+            )
+        )
+        dao.insertCart(*insertList)
+        dao.deleteCart(*insertList)
+        val cartList = dao.findAll().first()
+
         assertThat(cartList).isEmpty()
     }
 
