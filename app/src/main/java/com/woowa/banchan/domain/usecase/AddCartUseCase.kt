76f3dfa61a1b92f8252ca585cf23b5ac3c0ac8a5
bn-tw2020 <skaxodn97@gmail.com>
2022-08-18 18:10:46 +0900

    feat: 장바구니 추가 유스케이스
    - Dispatcher.IO 설정

--- app/src/main/java/com/woowa/banchan/domain/usecase/AddCartUseCase.kt
+++ app/src/main/java/com/woowa/banchan/domain/usecase/AddCartUseCase.kt
@@ -3,17 +3,19 @@ package com.woowa.banchan.domain.usecase
 import com.woowa.banchan.domain.entity.Cart
 import com.woowa.banchan.domain.exception.ExistCartException
 import com.woowa.banchan.domain.repository.CartRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.flow.flowOn
 
 class AddCartUseCase(
     private val cartRepository: CartRepository
 ) {
 
     operator fun invoke(cart: Cart) = flow {
-        when (cartRepository.isExistCart(cart.hash).single()) {
-            false -> emit(Result.failure<Throwable>(ExistCartException()))
-            true -> cartRepository.addCart(cart)
+        when (cartRepository.isExistCart(cart.hash).first()) {
+            false -> cartRepository.addCart(cart)
+            true -> emit(Result.failure<Throwable>(ExistCartException()))
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }
