94319d4b56702ccb8e03255383a7e02306c3db34
bn-tw2020 <skaxodn97@gmail.com>
2022-08-18 11:17:31 +0900

    feat: 주문과 주문항목을 조회할 뷰 생성
    - 데이터베이스 view 생성
    - order dao 메소드 생성

--- app/src/main/java/com/woowa/banchan/data/local/dao/OrderDao.kt
+++ app/src/main/java/com/woowa/banchan/data/local/dao/OrderDao.kt
@@ -1,10 +1,24 @@
 package com.woowa.banchan.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.woowa.banchan.data.local.entity.OrderEntity
+import com.woowa.banchan.data.local.entity.OrderLineItemEntity
+import com.woowa.banchan.data.local.entity.OrderLineItemView
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface OrderDao {
-    /*
-        findAll(): Flow<List<*>>
-     */
+
+    @Query("SELECT * FROM OrderLineItemView")
+    fun findAll(): Flow<List<OrderLineItemView>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrder(orderEntity: OrderEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrderLineItem(vararg orderLineItemEntity: OrderLineItemEntity)
+
 }
---
+++ app/src/main/java/com/woowa/banchan/data/local/entity/OrderLineItemView.kt
@@ -0,0 +1,19 @@
+package com.woowa.banchan.data.local.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.DatabaseView
+
+@DatabaseView(
+    """
+    SELECT a.id, a.ordered_at, a.status, b.name, b.image_url, b.quantity, b.price FROM `order` as a JOIN order_line_item as b ON a.id = b.order_id 
+"""
+)
+data class OrderLineItemView(
+    @ColumnInfo(name = "id") val id: Long,
+    @ColumnInfo(name = "ordered_at") val orderedAt: Long,
+    @ColumnInfo(name = "status") val status: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @ColumnInfo(name = "quantity") val quantity: Int,
+    @ColumnInfo(name = "price") val price: String
+)
