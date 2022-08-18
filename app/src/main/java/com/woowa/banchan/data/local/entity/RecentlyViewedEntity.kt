88d6e47e5561a3cdaf273bbbb4a0255c3216c06a
rosf73 <alswlzhsclq@naver.com>
2022-08-18 18:02:54 +0900

    feat: domain entity로 변환하기 위한 확장함수 작성
    - price 정보는 String으로 저장

--- app/src/main/java/com/woowa/banchan/data/local/entity/RecentlyViewedEntity.kt
+++ app/src/main/java/com/woowa/banchan/data/local/entity/RecentlyViewedEntity.kt
@@ -3,6 +3,7 @@ package com.woowa.banchan.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.woowa.banchan.domain.entity.RecentlyViewed
 
 @Entity(tableName = "recently_viewed")
 data class RecentlyViewedEntity(
@@ -10,7 +11,11 @@ data class RecentlyViewedEntity(
     @ColumnInfo(name = "hash") val hash: String,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "image_url") val imageUrl: String,
-    @ColumnInfo(name = "n_price") val nPrice: Int,
-    @ColumnInfo(name = "s_price") val sPrice: Int,
+    @ColumnInfo(name = "n_price") val nPrice: String,
+    @ColumnInfo(name = "s_price") val sPrice: String,
     @ColumnInfo(name = "viewed_at") val viewedAt: Long
 )
+
+fun RecentlyViewedEntity.toRecentlyViewed(): RecentlyViewed {
+    return RecentlyViewed(id, hash, name, imageUrl, nPrice, sPrice, viewedAt)
+}
