73dd3beed31bc26756a60830db35b077b7738976
bn-tw2020 <skaxodn97@gmail.com>
2022-08-20 01:46:39 +0900

    feat: 장바구니 repository 변경
    - suspend 처리
    
    - 장바구니 수정 기능
    
    - 장바구니 목록 리스트 가져오기(Map 으로 변경)

--- app/src/main/java/com/woowa/banchan/domain/repository/CartRepository.kt
+++ app/src/main/java/com/woowa/banchan/domain/repository/CartRepository.kt
@@ -4,8 +4,10 @@ import com.woowa.banchan.domain.entity.Cart
 import kotlinx.coroutines.flow.Flow
 
 interface CartRepository {
-    fun addCart(cart: Cart)
-    fun removeCart(vararg carts: Cart)
-    fun getCarts(): Flow<List<Cart>>
+    suspend fun addCart(cart: Cart)
+    suspend fun updateCart(vararg cart: Cart)
+    suspend fun removeCart(vararg carts: Cart)
+    fun getCarts(): Flow<Map<String, Cart>>
+    fun getCart(hash: String): Flow<Cart>
     fun isExistCart(hash: String): Flow<Boolean>
 }
