b4c8114d4979b01be8c8e7f5efc39c4d45f022e4
rosf73 <alswlzhsclq@naver.com>
2022-08-19 10:57:10 +0900

    ui: 0분 전 -> 방금

--- app/src/main/java/com/woowa/banchan/extensions/TimeStringConverter.kt
+++ app/src/main/java/com/woowa/banchan/extensions/TimeStringConverter.kt
@@ -6,17 +6,20 @@ fun Long.toTimeString(): String {
     val today = Calendar.getInstance()
     val target = Calendar.getInstance().apply { timeInMillis = this@toTimeString }
 
+    val diffSec = (today.time.time - target.time.time) / 1000
     val diffMin = (today.time.time - target.time.time) / 60 / 1000
     val diffHour = (today.time.time - target.time.time) / 60 / 60 / 1000
 
-    return if (diffHour < 1)
-               "${diffMin}분 전"
-           else if (diffHour < 24)
-               "${diffHour}시간 전"
-           else if (diffHour < 24 * 30)
-               "${diffHour / 24}일 전"
-           else if (diffHour < 24 * 30 * 12)
-               "${diffHour / (24 * 30)}달 전"
-           else
-               "${diffHour / (24 * 30 * 12)}년 전"
+    return if (diffSec < 60)
+            "방금"
+        else if (diffHour < 1)
+           "${diffMin}분 전"
+        else if (diffHour < 24)
+           "${diffHour}시간 전"
+        else if (diffHour < 24 * 30)
+           "${diffHour / 24}일 전"
+        else if (diffHour < 24 * 30 * 12)
+           "${diffHour / (24 * 30)}달 전"
+        else
+           "${diffHour / (24 * 30 * 12)}년 전"
 }
