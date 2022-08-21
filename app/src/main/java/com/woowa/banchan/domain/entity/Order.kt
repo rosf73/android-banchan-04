6590dbc8a4e521fe788d20d6260ddca7ff355124
bn-tw2020 <skaxodn97@gmail.com>
2022-08-21 12:51:51 +0900

    chore: 사용하지 않은 객체 제거
    - 데이터 클래스로 가지고 있던 정보(Order, OrderLineItem)을 sealed class로 넘기면서 제거

--- app/src/main/java/com/woowa/banchan/domain/entity/Order.kt
+++
@@ -1,7 +0,0 @@
-package com.woowa.banchan.domain.entity
-
-data class Order(
-    val id: Long = 0,
-    val orderedAt: Long,
-    val status: String
-)
--- app/src/main/java/com/woowa/banchan/domain/entity/OrderLineItem.kt
+++
@@ -1,8 +0,0 @@
-package com.woowa.banchan.domain.entity
-
-data class OrderLineItem(
-    val name: String,
-    val imageUrl: String,
-    val quantity: Int,
-    val price: String
-)
