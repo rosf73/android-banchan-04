20d8b336764b172204a4db1a1addc23769d536b1
rosf73 <alswlzhsclq@naver.com>
2022-08-23 01:42:42 +0900

    fix: 가로 스크롤 상에서 세로 스크롤이 안되던 문제 해결
    - 네이밍 Horizontal -> Nested
    - 의미없지만 fragment_home 필요없는 layout wrap 제거

---
+++ app/src/main/java/com/woowa/banchan/ui/main/tabs/home/NestedScrollListener.kt
@@ -0,0 +1,43 @@
+package com.woowa.banchan.ui.main.tabs.home
+
+import android.view.MotionEvent
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+
+class NestedScrollListener : RecyclerView.OnItemTouchListener {
+
+    private var lastX: Float = 0f
+    private var lastY: Float = 0f
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        if (rv.canScrollHorizontally(1) || rv.canScrollHorizontally(-1))
+            when (e.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    lastX = e.x
+                    lastY = e.y
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val currentX = e.x
+                    val currentY = e.y
+                    val dx = abs(currentX - lastX)
+                    val dy = abs(currentY - lastY)
+                    if (dy > dx)
+                        rv.parent
+                            .requestDisallowInterceptTouchEvent(false)
+                    else
+                        rv.parent
+                        .requestDisallowInterceptTouchEvent(true)
+                }
+                MotionEvent.ACTION_UP ->
+                    rv.parent
+                        .requestDisallowInterceptTouchEvent(false)
+            }
+        else
+            rv.parent
+                .requestDisallowInterceptTouchEvent(false)
+        return false
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+}
