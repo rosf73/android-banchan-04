902aee80ceea89c77df5e0d0f15494b609adf1dc
rosf73 <alswlzhsclq@naver.com>
2022-08-22 18:22:11 +0900

    feat: mainactivity에서 항상 감지하도록 설정
    - app launch마다 flow로부터 unconnected가 감지되지 않는 문제 임시 해결

--- app/src/main/java/com/woowa/banchan/ui/MainActivity.kt
+++ app/src/main/java/com/woowa/banchan/ui/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.woowa.banchan.ui
 
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.woowa.banchan.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnBackClickListener {
+
+    private lateinit var connectivityObserver: ConnectivityObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
         setContentView(R.layout.activity_main)
+
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (cm.activeNetwork == null) {
+            finishAffinity()
+        }
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                connectivityObserver.observe().collect {
+                    if (it == ConnectivityObserver.Status.Available) {
+
+                    } else {
+                        finishAffinity()
+                    }
+                }
+            }
+        }
     }
 
     override fun navigateToBack() {
