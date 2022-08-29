19924f3cc5f911aa8efe2580f6e7c4d59b19d042
rosf73 <alswlzhsclq@naver.com>
2022-08-29 14:25:48 +0900

    chore: 버전 코드 수정

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -4,7 +4,7 @@ object Config {
     const val applicationId = "com.woowa.banchan"
     const val targetSdk = 32
     const val minSdk = 21
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "1.1"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
